4d41050b8943d37cb92dda4f3c25a70c0f232be5
Damyan Doykov <77811406+Damyanmd@users.noreply.github.com>
2021-05-21 09:25:24 +0300

    Add files via upload

---
+++ OOP/Defining classes/hero.py
@@ -0,0 +1,15 @@
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def defend(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.health = 0
+            return f'{self.name} was defeated'
+        else:
+            return 'None'
+
+    def heal(self, amount):
+        self.health += amount
